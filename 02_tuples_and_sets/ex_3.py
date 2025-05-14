339ad9ec6d590a284b9416c4b37c3124ff4d9c17
Ines Ivanova Kenova <ines.ivanova@nmible.com>
2025-05-14 20:29:47 +0300

    lecture 2

---
+++ 02_tuples_and_sets/ex_3.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+
+names = set()
+
+for _ in range(n):
+    name = input()
+    names.add(name)
+
+
+print(*names, sep='\n')
---
+++ main.py
@@ -0,0 +1,47 @@
+# a = set([3, 4])
+# b = set([3, 4])
+#
+#
+# # print(a | b)
+# # print(a.union(b))
+# #
+# # print(a & b)
+# # print(a.intersection(b))
+# #
+# # print(a - b)
+# # print(a.difference(b))
+# #
+# # print(b - a)
+# # print(b.difference(a))
+#
+# print(a ^ b)
+# print(a.symmetric_difference(b))
+#
+# #
+# # print(a < b)
+# # print(a.issubset(b))
+# #
+# # print(a > b)
+# # print(a.issuperset(b))
+# #
+# # print(b > a)
+#
+# names = {}
+# print(type(names))
+#
+#
+# a = [1, 2, 3, 4, 2, 4]
+#
+# nums = {el for el in a if el % 2 == 0}
+# print(nums)
+
+
+nums = {1, 2, 3}
+nums2 = list(nums)
+#
+# nums.remove(4)
+
+print(nums)
+
+words = ["asd", "ASD", "ASd", "6as"]
+print(sorted(words, reverse=True))
