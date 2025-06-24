0b61cbe1e6416b126c1b17a46b8c3e9d4c4fcd71
Ines Ivanova Kenova <ines.ivanova@nmible.com>
2025-06-24 08:22:08 +0300

    first steps in OOP

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from random import  choice, shuffle as r_shuffle
-fruits = ["apple", "banana", "cherry"]
+# class Person:
+#     def __init__(self, name):
+#         self.name = name
+#
+#
+#
+#
+#
+# class Greeter:
+#     def greet(self, person: Person):
+#         print(f"Hi, {person.name}!")
+#
+#
+# greeter = Greeter()
+# person = Person("Test")
+# greeter.greet(person)
 
 
-def shuffle():
-    return "Hello"
+class Animal:
+    def __init__(self, name):
+        self.name = name
 
-print(choice(fruits))
-r_shuffle(fruits)
-print(fruits)
-print(shuffle())
+    def sleep(self):
+        return f"the animal with name {self.name} is sleeping.."
+
+    def eat(self):
+        return f"the animal with name is eating."
+
+animal = Animal("cat")
+print(animal.name)
+print(animal.sleep()) # sleeping..
+
